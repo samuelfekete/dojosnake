b7dbf3fc37e884b100b8eebe731602f668acbd65
Samuel Fekete <samuel.fekete@gmail.com>
2020-01-11 23:02:34 +0000

    clean up

--- snake.py
+++ snake.py
@@ -8,7 +8,7 @@ UP = 180
 LEFT = 270
 DOWN = 360
 
-food = Actor('food_tiny', center=(random.randrange(WIDTH), random.randrange(HEIGHT)))
+food = Actor('food_tiny', center=(WIDTH/2, HEIGHT/2))
 snake_head = Actor('snake_head_tiny', left=0, top=0)
 snake = [snake_head]
 
@@ -37,28 +37,37 @@ def draw():
 
 def snake_is_colliding():
     """Returns True if the snake collided with itself"""
+    
     if snake_head.collidelist(snake[1:]) > 0:
         return True
+    
     if snake_over_edge():
         return True
 
 def snake_is_eating():
     """Returns True if the snake is eating food"""
+    
     if snake_head.colliderect(food):
         return True
 
 def snake_over_edge():
     """Returns true if the snake is over the edge."""
+    
     if snake_head.left < 0:
         return True
+    
     if snake_head.top < 0:
         return True
+    
     if snake_head.right > WIDTH:
         return True
+    
     if snake_head.bottom > HEIGHT:
         return True
 
 def move_snake():
+    """Move the snake to the next position."""
+
     for i in range(len(snake) - 1, 0, -1):
         snake[i].center = snake[i-1].center
 
@@ -81,22 +90,29 @@ def move_snake():
         grow_snake()
         animate(
             food,
-            pos=(random.randrange(20, WIDTH-20), random.randrange(20,HEIGHT-20)),
+            pos=(
+                random.randrange(food.width, WIDTH - food.width),
+                random.randrange(food.width, HEIGHT - food.width)),
             tween="in_out_elastic",
             duration=0.5,
         )
 
 def on_key_down(key, *args):
+    """Handle a key being pressed."""
+    
     if key == keys.DOWN:
         snake_head.direction = DOWN
         snake_head.angle = DOWN
-    elif key == keys.UP:
+    
+    if key == keys.UP:
         snake_head.direction = UP
         snake_head.angle = UP
-    elif key == keys.LEFT:
+    
+    if key == keys.LEFT:
         snake_head.direction = LEFT
         snake_head.angle = LEFT
-    elif key == keys.RIGHT:
+    
+    if key == keys.RIGHT:
         snake_head.direction = RIGHT
         snake_head.angle = RIGHT
 
